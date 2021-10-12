ab26a3b55107bc3802651eb67893cb9899757b1a
Natnael <natnaelebuy@gmail.com>
2021-10-11 22:29:28 -0700

    update

--- testMath.java
+++ testMath.java
@@ -14,7 +14,12 @@ public class testMath {
                 System.out.println( myFloat[i] - ((myFloat[i] % step)));{
              
              
-            
+            /// creating another float type 
+
+            float num1=5.5f;  
+            float num2=5f;  
+            System.out.println("num1: "+num1);  
+            System.out.println("num2: "+num2);  
              
              
                 }
