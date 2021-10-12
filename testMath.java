72b1764757d6c4aec9224c9f618bab66430472de
Natnael <natnaelebuy@gmail.com>
2021-10-11 22:08:14 -0700

    update changing numbers

--- testMath.java
+++ testMath.java
@@ -3,7 +3,7 @@ public class testMath {
     public static void main(String[] args) {
          
         Float step = 0.25f;
-        Float[] myFloat = {2.0f, 2.125f, 2.25f , 2.375f, 2.5f , 2.625f , 2.75f , 2.875f , 3.0f };
+        Float[] myFloat = {2.0f, 2.165f, 2.28f , 2.456f, 2.6f , 2.7f , 2.759f , 2.875f , 3.0f };
      
         for (int i=0; i< myFloat.length; i++) {
             System.out.print(myFloat[i] + "\t");
